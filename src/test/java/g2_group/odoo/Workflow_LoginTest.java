e902f22eeaf988403ed32ed9dceac953a032fd38
Abdulhakeem Sakhel <abodhakeemfabl@gmail.com>
2026-06-01 19:02:46 +0300

    refactored login email & password

--- src/test/java/g2_group/odoo/Workflow_LoginTest.java
+++ src/test/java/g2_group/odoo/Workflow_LoginTest.java
@@ -5,10 +5,14 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import g2_group.odoo.util.RandomStringUtil;
+
 public class Workflow_LoginTest extends BaseTest {
     private final String logInPath = "/web/login";
     private LoginPage loginPage;
 
+    private String email = ConfigReader.getProperty("email");
+    private String password = ConfigReader.getProperty("password");
     @Override
     protected String getPath() {
         return logInPath;
@@ -27,21 +31,19 @@ public class Workflow_LoginTest extends BaseTest {
 
     @Test(priority = 1)
     public void TC_LG_01_SuccessfulLogin() {
-        String email = ConfigReader.getProperty("email");
-        String password = ConfigReader.getProperty("password");
         loginPage.loginFromUI(email, password);
         Assert.assertTrue(loginPage.isLoggedIn(), "TC_LG_01 Failed: Could not login with valid credentials.");
     }
 
     @Test(priority = 2)
     public void TC_LG_02_LoginWithInvalidPassword() {
-        loginPage.loginFromUI("abodbarca18@gmail.com", "WrongPassword123");
+        loginPage.loginFromUI(email, "WrongPassword123");
         Assert.assertFalse(loginPage.isLoggedIn(), "TC_LG_02 Failed: Logged in with wrong password.");
     }
 
     @Test(priority = 3)
     public void TC_LG_03_LoginWithInvalidUsername() {
-        loginPage.loginFromUI("fakeuser@gmail.com", "ab548220-*");
+        loginPage.loginFromUI("fakeuser@gmail.com", password);
         Assert.assertFalse(loginPage.isLoggedIn(), "TC_LG_03 Failed: Logged in with wrong username.");
     }
 
@@ -53,32 +55,32 @@ public class Workflow_LoginTest extends BaseTest {
 
     @Test(priority = 5)
     public void TC_LG_05_LoginWithBlankPasswordOnly() {
-        loginPage.loginFromUI("abodbarca18@gmail.com", "");
+        loginPage.loginFromUI(email, "");
         Assert.assertFalse(loginPage.isLoggedIn(), "TC_LG_05 Failed: Allowed login with blank password.");
     }
 
     @Test(priority = 6)
     public void TC_LG_06_LoginWithCaseSensitivePassword() {
-        loginPage.loginFromUI("abodbarca18@gmail.com", "AB548220-*");
+        loginPage.loginFromUI(email, password.toUpperCase());
         Assert.assertFalse(loginPage.isLoggedIn(), "TC_LG_06 Failed: Password is not case-sensitive.");
     }
 
     @Test(priority = 7)
     public void TC_LG_07_LoginWithExtraWhitespaceInUsername() {
-        loginPage.loginFromUI("  abodbarca18@gmail.com  ", "ab548220-*");
+        loginPage.loginFromUI(" ".repeat(3) + email + " ".repeat(5), password);
         Assert.assertFalse(loginPage.isLoggedIn(), "TC_LG_07 Failed: Allowed login with whitespaces.");
     }
 
     @Test(priority = 8)
     public void TC_LG_08_LoginWithSQLInjectionPayload() {
-        loginPage.loginFromUI("' OR '1'='1", "ab548220-*");
+        loginPage.loginFromUI("' OR '1'='1", password);
         Assert.assertFalse(loginPage.isLoggedIn(), "TC_LG_08 Failed: System vulnerable to SQL Injection.");
     }
 
     @Test(priority = 9)
     public void TC_LG_09_LoginWithMaximumCharacterBoundary() {
-        String longEmail = "A".repeat(240) + "@gmail.com";
-        loginPage.loginFromUI(longEmail, "ab548220-*");
+        String longEmail =  RandomStringUtil.randomString(240) + "@gmail.com";
+        loginPage.loginFromUI(longEmail, password);
         Assert.assertFalse(loginPage.isLoggedIn(), "TC_LG_09 Failed: System allowed login with invalid long boundary email.");
     }
 }
